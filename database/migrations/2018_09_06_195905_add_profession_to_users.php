439542c5e7332a38a1f07af274633ed04c1d7ad7
Jorge Solis <jorge.solis@cceo.com.mx>
2018-09-06 15:34:22 -0500

    Fix table users

--- database/migrations/2018_09_06_195905_add_profession_to_users.php
+++ database/migrations/2018_09_06_195905_add_profession_to_users.php
@@ -14,7 +14,7 @@ class AddProfessionToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profession')->nullable();
+            $table->string('profession')->nullable()->after('password');
         });
     }
 
