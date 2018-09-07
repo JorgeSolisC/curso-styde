cd08dce2df4c0ebf3e5b3ae11a9d42f9e50e5021
Jorge Solis <jorge.solis@cceo.com.mx>
2018-09-07 16:25:44 -0500

    Add Seeders

--- database/migrations/2018_09_06_215217_add_profession_id_to_users.php
+++ database/migrations/2018_09_06_215217_add_profession_id_to_users.php
@@ -14,7 +14,7 @@ class AddProfessionIdToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('profession_id');
+            $table->unsignedInteger('profession_id')->nullable();
             //el cmapo profession_id hara referencia al campo id de la tabla professions
             $table->foreign('profession_id')->references('id')->on('professions');
         });
