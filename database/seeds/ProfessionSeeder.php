47d55177d2a1bc2d9d19b62c18bf42a96e53083b
Jorge Solis <jorge.solis@cceo.com.mx>
2018-09-11 16:34:21 -0500

    Create Model Profession for Eloquent ORM

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Profession;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,8 +15,16 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
+        //metodo inset sql
+        //DB::insert('INSERT INTO professions (title) VALUES ("Desarrollardor back-end")');
 
-        DB::table('professions')->insert([
+        //metodo insert con parametros dinamico protege contra la inyeccion de sql
+        //DB::insert('INSERT INTO professions (title) VALUES (?)', ['Desarrollador back-end']);
+        //DB::insert('INSERT INTO professions (title) VALUES (:title)', ['title'=>'Desarrollador back-end']);
+
+
+        //Metodo constructor de consultas por sql laravel
+/*         DB::table('professions')->insert([
             'title' => 'Desarrollador back-end',
         ]);
 
@@ -24,6 +34,20 @@ class ProfessionSeeder extends Seeder
 
         DB::table('professions')->insert([
             'title' => 'Diseñador web',
+        ]); */
+
+        //Ver insertar datos de Eloquent
+
+        Profession::create([
+            'title' => 'Desarrollador back-end',
+        ]);
+
+        Profession::create([
+            'title'=> 'Desarrollador front-end'
+        ]);
+
+        Profession::create([
+            'title'=> 'Diseñador web'
         ]);
     }
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Profession;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -12,12 +15,67 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $professionId = Profession::where('title','Desarrollador back-end')->value('id');
+
+        User::create([
+            // ..
+            'name'=>'CURCO',
+            'email'=>'cur@example.com',
+            'password' => bcrypt('laravel'),
+            'profession_id' => $professionId,
+            'is_admin'=> true,
+        ]);
+
+        User::create([
+            // ..
+            'name'=>'User2',
+            'email'=>'c@example.com',
+            'password' => bcrypt('laravel'),
+            'profession_id' => $professionId,
+        ]);
+
+        User::create([
+            // ..
+            'name'=>'User3',
+            'email'=>'cu@example.com',
+            'password' => bcrypt('laravel'),
+            'profession_id' => null,
+        ]);
+
+        //método DB::select podemos construir una consulta SELECT de SQL de forma manual:
+        //$professions = DB::select('SELECT id FROM professions WHERE title = ?', ['Desarrollador back-end']);
+
+            //constructor de consultas podemos realizar una consulta SQL
+        //$profession = DB::table('professions')->select('id')->take(1)->get();
+        //$profession = DB::table('professions')->select('id')->first();
+
+            //Consultas con condicionales (WHERE)
+        //$profession = DB::table('professions')->select('id')->where('title', '=', 'Desarrollador back-end')->first();
+            //El operador = dentro del método where es opcional.
+        //$profession = DB::table('professions')->select('id')->where('title', 'Desarrollador back-end')->first();
+            //El método where también acepta un array asociativo,
+        //$profession = DB::table('professions')->select('id')->where(['title' => 'Desarrollador back-end'])->first();
+
+            //Omitiendo el método select al utilizar DB::table podemos retornar todas las columnas:
+        // $profession = DB::table('professions')->where('title', '=', 'Desarrollador back-end')->first();
+
+            //Métodos dinámicos
+        /*$professionId = DB::table('professions')
+        ->where('title','Desarrollador back-end')
+        -> value('id');
+        $professionId = DB::table('professions')
+        ->whereTitle('Desarrollador back-end')->first();
+
+        //$professions->first(); // en vez de $professions[0]
+        dd($professionId);
 
         DB::table('users')->insert([
             // ..
             'name'=>'CURCO',
             'email'=>'cur@example.com',
             'password' => bcrypt('laravel'),
-        ]);
+            'profession_id' => $professionId,
+        ]);*/
+
     }
 }
